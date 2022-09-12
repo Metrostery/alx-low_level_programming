de43d7817e652049eb343fead11313bfb0b2f9aa
metrostery <obiezuchibuike@gmail.com>
2022-09-12 15:04:39 -0700

    Learning c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * *main- print the string with the printf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a peice of art,\n");
+	return (0);
+}
