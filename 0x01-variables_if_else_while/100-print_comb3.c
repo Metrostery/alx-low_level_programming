356654d7b53abeb78873daca3d7e7a0310350275
metrostery <obiezuchibuike@gmail.com>
2022-09-15 08:19:32 -0700

    Learning c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
+ * main - program that prints all possible different combinations of 2 digits
  *
- *  * main - program that prints all possible different combinations of 2 digits
- *
- *   * Return: 0
- *
- *    */
+ * Return: 0
+ */
 
 int main(void)
 {
