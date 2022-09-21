44a83349549f59543f352052126a62a748624362
metrostery <obiezuchibuike@gmail.com>
2022-09-21 07:56:00 -0700

    Learning c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - prints out letters
+ *
+ * Description: using new function
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i<10)
+	{
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+	i++;
+}
