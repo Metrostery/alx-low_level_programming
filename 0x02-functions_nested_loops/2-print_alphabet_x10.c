2a626d7ed7932fca87f05a57e2f66aa046382fe4
metrostery <obiezuchibuike@gmail.com>
2022-09-21 08:29:32 -0700

    Learning C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,17 +10,16 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	int i;
+	char ch;
 
 	while (i < 10)
 	{
-		char ch;
-
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\n');
-	i++;
 }
