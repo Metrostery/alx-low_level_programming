029dbd0959e484626a376c48d7e23ad77cb23931
metrostery <obiezuchibuike@gmail.com>
2022-09-22 14:55:34 -0700

    Learning C

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int main (void)
 
 	printf("Infinite loop incoming :(\n");
 	
-	i = 0
-		/*
-		 * while (i < 10)
-		 * {
-		 * _putchar(i);
-		 * }
-		 */
-		printf("Infinite loop avoided! \\o/\n");
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 * _putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
