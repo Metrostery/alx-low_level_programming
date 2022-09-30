5d23c50f01dc0b524a82d773623995dfe76d7aee
metrostery <obiezuchibuike@gmail.com>
2022-09-30 01:34:07 -0700

    Learning c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - puts half of a string
+ * @str: the string
+ *
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		i = i / 2;
+	}
+	else
+	{
+		i = (i + 1) / 2;
+	}
+	while (str[i] != '\0')
+	{
+		j = str[i];
+		j++;
+		_putchar(j);
+	}
+	_putchar('\n');
+}
