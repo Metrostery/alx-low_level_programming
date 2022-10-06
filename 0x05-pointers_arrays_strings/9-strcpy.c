07e36fc703f5405ea8d79105c3fd50cedf82c990
metrostery <obiezuchibuike@gmail.com>
2022-10-06 03:09:33 -0700

    Learning c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @dest: array that returns
+ * @src: pointers that recieve the string
+ *
+ * Return: dest
+ */
+
+void *_strcpy(int *dest, int *src)
+{
+	int n = 0;
+	int i;
+
+	while(src[n] != '\0')
+	{
+		n++;
+	}
+
+	i = 0;
+	while(i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	i = i;
+	while(i <= n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
